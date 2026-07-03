7ebdb90360fd7f62636485a67b6bfab43063c949
Lyco-creator <ehuot92@gmail.com>
2026-07-03 13:52:21 +0200

    test level2

---
+++ src/data/Level2Layout.js
@@ -0,0 +1,10 @@
+export const level2Layout = {
+  playerSpawn: { x: 70, y: 360 },
+  fires: [
+    { x: 100, y: 200, size: 'small', type: 'normal' }
+  ],
+  doors: [
+    { x: 200, y: 300, isDangerous: false },
+    { x: 500, y: 150, isDangerous: true }
+  ],
+}
